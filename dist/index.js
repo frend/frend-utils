5cfff7e4e1fef82259097e71abc46a735d9993ae
thomasdigby <thomasdigby1@gmail.com>
2016-09-26 16:25:33 +0100

    Added commonJS output

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,20 @@
-var matches = function matches(el, selector) {
-  // update prototype to match browser vendors
-  Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.mozMatchesSelector || Element.prototype.webkitMatchesSelector;
-  // detect matches support and return native function
-  if (typeof Element.prototype.matches === 'function') return el.matches(selector);
-  // if not supported, run fallback function
-  var elems = (el.document || el.ownerDocument).querySelectorAll(selector);
-  var i = 0;
-  while (elems[i] && elems[i] !== el) {
-    ++i;
-  }return Boolean(elems[i]);
-};
+'use strict';
 
-var closest = function closest(el, selector) {
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var closest = (function (el, selector) {
+  var matches = function matches(matchEl, matchSelector) {
+    // update prototype to match browser vendors
+    Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.mozMatchesSelector || Element.prototype.webkitMatchesSelector;
+    // detect matches support and return native function
+    if (typeof Element.prototype.matches === 'function') return matchEl.matches(matchSelector);
+    // if not supported, run fallback function
+    var elems = (matchEl.document || matchEl.ownerDocument).querySelectorAll(matchSelector);
+    var i = 0;
+    while (elems[i] && elems[i] !== matchEl) {
+      ++i;
+    }return Boolean(elems[i]);
+  };
   // detect closest support and return native function
   if (typeof Element.prototype.closest === 'function') return el.closest(selector);
   // if not supported, run fallback function
@@ -21,7 +24,7 @@ var closest = function closest(el, selector) {
     tempEl = tempEl.parentNode;
   }
   return null;
-};
+});
 
 var defer = (function (fn) {
   //  defers invoking the function until the current call stack has cleared
@@ -33,4 +36,6 @@ var nodelistArray = (function (el) {
   return [].slice.call(ctx.querySelectorAll(el));
 });
 
-export { closest, defer, nodelistArray as q };
+exports.closest = closest;
+exports.defer = defer;
+exports.q = nodelistArray;
